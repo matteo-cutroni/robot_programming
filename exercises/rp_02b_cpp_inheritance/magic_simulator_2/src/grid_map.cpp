f445cbb6063d310d09fd68d429b211a9cee20626
Emanuele Giacomini <emanuele.giacomo@gmail.com>
2023-10-24 01:41:51 +0200

    Last minute bugfixes

--- exercises/rp_02b_cpp_inheritance/magic_simulator_2/src/grid_map.cpp
+++ exercises/rp_02b_cpp_inheritance/magic_simulator_2/src/grid_map.cpp
@@ -12,17 +12,18 @@ GridMap::GridMap(int rows_, int cols_, Scalar resolution_) {
 
 GridMap::~GridMap() { delete[] values; }
 
-bool GridMap::scanRay(Vec2& hit, const Vec2& origin, const Vec2& direction, const Scalar max_range) {
-  Scalar range=0;
-  while (range<max_range) {
-    hit = origin + direction*range;
-    Vec2i int_endpoint=world2grid(hit);
-    range+=resolution;
-    if (!inside(int_endpoint))
-      return false;
-    uint8_t val=at(int_endpoint[1], int_endpoint[0]);
+bool GridMap::scanRay(Vec2& hit, const Vec2& origin, const Vec2& direction,
+                      const Scalar max_range) {
+  Scalar range = 0;
+  while (range < max_range) {
+    hit = origin + direction * range;
+    Vec2i int_endpoint = world2grid(hit);
+    range += resolution;
+    if (!inside(int_endpoint)) return false;
+    uint8_t val = at(int_endpoint[1], int_endpoint[0]);
     // cerr << "range: " << range << " ep: " << hit[0] << " " << hit[1]
-    //      << " iep: " << int_endpoint[0] << " " << int_endpoint[1] << " " << (int) val << endl; 
+    //      << " iep: " << int_endpoint[0] << " " << int_endpoint[1] << " " <<
+    //      (int) val << endl;
     if (val < 127) {
       return true;
     }
@@ -30,40 +31,27 @@ bool GridMap::scanRay(Vec2& hit, const Vec2& origin, const Vec2& direction, cons
   return false;
 }
 
-void GridMap::loadFromImage(const char *filename, Scalar res){
-  resolution=res;
-  inv_resolution=1./res;
+void GridMap::loadFromImage(const char* filename, Scalar res) {
+  resolution = res;
+  inv_resolution = 1. / res;
   cerr << "loading [" << filename << "]" << endl;
-  cv::Mat m=cv::imread(filename);
-  if (m.rows==0) {
+  cv::Mat m = cv::imread(filename);
+  if (m.rows == 0) {
     throw std::runtime_error("unable to load image");
   }
   cv::Mat loaded_image;
   cv::cvtColor(m, loaded_image, cv::COLOR_BGR2GRAY);
-  int size=loaded_image.rows*loaded_image.cols;
-  values=new uint8_t[loaded_image.rows*loaded_image.cols];
-  rows=loaded_image.rows;
-  cols=loaded_image.cols;
-  origin[0]=cols/2;
-  origin[1]=rows/2;
-  
+  int size = loaded_image.rows * loaded_image.cols;
+  values = new uint8_t[loaded_image.rows * loaded_image.cols];
+  rows = loaded_image.rows;
+  cols = loaded_image.cols;
+  origin[0] = cols / 2;
+  origin[1] = rows / 2;
+
   memcpy(values, loaded_image.data, size);
 }
 
 void GridMap::draw(Canvas& dest) {
-  dest=cv::Mat(rows, cols, CV_8UC1);
-  memcpy(dest.data, values, rows*cols);
-}
-
-void drawLine(Canvas& dest, const Vec2i& p0, const Vec2i& p1, uint8_t color) {
-  cv::line(dest, cv::Point(p0[0], p0[1]), cv::Point(p1[0], p1[1]), cv::Scalar(color, color, color), 1);
-}
-
-int showCanvas(Canvas& canvas, int timeout_ms) {
-  cv::imshow("canvas", canvas);
-  int key=cv::waitKey(timeout_ms);
-  if (key==27) // exit on ESC
-    exit(0);
-  //cerr << "key" << key << endl;
-  return  key;
+  dest = cv::Mat(rows, cols, CV_8UC1);
+  memcpy(dest.data, values, rows * cols);
 }
